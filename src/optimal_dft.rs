8148fc3b2d9ee9a2a94b841ae76f963fb84ccc3f
Anton <anton@Antons-MacBook-Air.local>
2025-07-24 18:20:52 +0300

    Updates

--- src/optimal_dft.rs
+++ src/optimal_dft.rs
@@ -1,17 +1,22 @@
-pub static MAX: usize = 360;
+pub const MAX: usize = 360;
+pub const MIN: usize = 10;
 
-static OPTIMAL_DFT_SIZE_TAB: &[usize] = &[
-    1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36, 40, 45, 48,
-    50, 54, 60, 64, 72, 75, 80, 81, 90, 96, 100, 108, 120, 125, 128, 135, 144, 150, 160,
-    162, 180, 192, 200, 216, 225, 240, 243, 250, 256, 270, 288, 300, 320, 324, MAX
+const SIZES: &[usize] = &[
+    MIN, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36, 40, 45, 48,
+    50, 54, 60, 64, 72, 75, 80, 81, 90, 96, 100, 108, 120, 125, 
+    128, 135, 144, 150, 160, 162, 180, 192, 200, 216, 225, 240, 
+    243, 250, 256, 270, 288, 300, 320, 324, MAX,
 ];
 
-pub static LEN: usize = OPTIMAL_DFT_SIZE_TAB.len(/**/);
+pub const LEN: usize = SIZES.len(/**/);
 
 pub fn get_size(size: usize) -> usize {
-    match OPTIMAL_DFT_SIZE_TAB.binary_search(&size) {
-        Err(index) if index < LEN => OPTIMAL_DFT_SIZE_TAB[index],
-        Ok(index) => OPTIMAL_DFT_SIZE_TAB[index],
-        _ => MAX
-    }
+    if size <= MIN { return MIN; }
+    if size >= MAX { return MAX; }
+
+    let idx = SIZES
+        .binary_search(&size)
+        .unwrap_or_else(|idx| idx);
+
+    SIZES[idx]
 }
